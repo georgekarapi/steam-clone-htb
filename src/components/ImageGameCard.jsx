963f43c132976b9270ee155574c2bd4d8a3a1dd6
Giorgos Karapiperidis <georgekarapi@yahoo.gr>
2023-01-26 14:23:43 +0200

    Overlay added to centered slides

--- src/components/ImageGameCard.jsx
+++ src/components/ImageGameCard.jsx
@@ -3,14 +3,14 @@ import PlatformIcons from './PlatformIcons';
 
 const ImageGameCard = ({ game }) => {
   return (
-    <div className="relative">
+    <div className="relative cursor-pointer">
       <img
         className="object-cover rounded-md border-[1px] border-neutral-800"
         src={game.header_image}
         alt={game.name}
         loading="lazy"
       />
-      <div className="w-full h-full absolute top-0 opacity-0 hover:opacity-90 bg-neutral-800 cursor-pointer">
+      <div className="overlay w-full h-full absolute top-0 invisible opacity-90 bg-neutral-800 hover:visible">
         <div className="flex flex-col gap-2 justify-end m-4">
           <div>{game.name}</div>
           <div>{<PlatformIcons platforms={game.platforms} />}</div>
